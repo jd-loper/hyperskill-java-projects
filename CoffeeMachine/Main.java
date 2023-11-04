57a3bc1251c085fd45c4c2e747232cdff188f9cd
Joseph Loper <jd.loper@icloud.com>
2023-11-04 06:48:31 -0700

    Print output for stage 1

---
+++ CoffeeMachine/Main.java
@@ -0,0 +1,15 @@
+package CoffeeMachine;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("""
+                Starting to make a coffee
+                Grinding coffee beans
+                Boiling water
+                Mixing boiled water with crushed coffee beans
+                Pouring coffee into the cup
+                Pouring some milk into the cup
+                Coffee is ready!
+                """);
+    }
+}
